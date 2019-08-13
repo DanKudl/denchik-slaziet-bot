91b16a43a6b8e6d616c2201208205b2256acd107
DanKudl <dans.pkthe,best@gmail.com>
2019-08-13 16:21:52 +0300

    3

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ const Token = '898108695:AAG4Fq7DHHTNrBVj8ZXrm0uqtNTZmMJT5fY'
 
 const bot = new TelegramBot ( Token, {polling: true})
 
-bot.on('message', msg =>  bot.sendMessage(msg.chat.id,'yea'))
+bot.on('message', msg =>  bot.sendMessage(msg.chat.id,'ага'))
+bot.start((ctx) => ctx.reply('Здраствуй!'))
