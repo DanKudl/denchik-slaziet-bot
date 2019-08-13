4626afa442a8bf20b7a76c09145b4b51a118db41
DanKudl <dans.pkthe,best@gmail.com>
2019-08-13 16:11:19 +0300

    2

--- index.js
+++ index.js
@@ -4,6 +4,4 @@ const Token = '898108695:AAG4Fq7DHHTNrBVj8ZXrm0uqtNTZmMJT5fY'
 
 const bot = new TelegramBot ( Token, {polling: true})
 
-bot.on('message', msg => {
-  bot.sendMessage(msg.chat.id, 'Hello from HEROKU, bot says: "Hi, $(msg.from.first_name)"'')
-})
+bot.on('message', msg =>  bot.sendMessage(msg.chat.id,'yea'))
