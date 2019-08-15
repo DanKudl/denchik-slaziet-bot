a268da1677b765f293c602c128fd2cfc2d71f5ab
DanKudl <dans.pkthe,best@gmail.com>
2019-08-15 10:06:47 +0300

    bot code

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const TelegramBot = require ('node-telegram-bot-api')
+const Telegraf = require('telegraf');
+const bot = new Telegraf('898108695:AAG4Fq7DHHTNrBVj8ZXrm0uqtNTZmMJT5fY');
 
-const Token = '898108695:AAG4Fq7DHHTNrBVj8ZXrm0uqtNTZmMJT5fY'
-
-const bot = new TelegramBot ( Token, {polling: true})
-
-bot.on('message', msg =>  bot.sendMessage(msg.chat.id,'ага'))
-bot.on('sticker', msg =>  bot.sendMessage(msg.chat.id,'мм стикер'))
+bot.start((ctx)=> {
+  ctx.reply ('Дарова поц!')
+})
+bot.help((ctx)=> {
+  ctx.reply ('Сам себе помагай')
+})
+bot.on ('sticker', (ctx) => ctx.reply('^-^'))
+bot.hears('Привет', (ctx) => ctx.reply('да да'))
+bot.hears('пинг', (ctx) =>  ctx.reply('понг'))
+bot.hears('Пинг', (ctx) =>  ctx.reply('как всегда высокий'))
+bot.hears('Ты кто', (ctx) =>  ctx.reply('Красаунчик'))
+bot.hears('я', (ctx) =>  ctx.reply('банан'))
+bot.launch()
