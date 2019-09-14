484ea070e40a23baa2771d395280dd1bc25bed89
DanKudl <dans.pkthe,best@gmail.com>
2019-09-14 10:17:30 +0300

    azaza

--- index.js
+++ index.js
@@ -4,5 +4,11 @@ const Token = '898108695:AAG4Fq7DHHTNrBVj8ZXrm0uqtNTZmMJT5fY'
 
 const bot = new TelegramBot ( Token, {polling: true})
 
-bot.on('message', msg =>  bot.sendMessage(msg.chat.id,'ага'))
+//bot.on('message', msg =>  bot.sendMessage(msg.chat.id,'ага'))
 bot.on('sticker', msg =>  bot.sendMessage(msg.chat.id,'мм стикер'))
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your message');
+});
